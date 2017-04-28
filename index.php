2b84fc05d74927df460755fa9b8886456a3929e7
Oscar Garcia <oscar.github@linaresdigital.com>
2017-04-28 10:58:32 +0200

    Fix PSR-2 coding style and some fixes/improvements

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php 
-if ($_POST)
-{
-	$i=1;
-    foreach ($_FILES['fileImage']['name'] as $f)
-		{
-			$file['name'] = $_FILES['fileImage']['name'][$i];
-			$file['type'] = $_FILES['fileImage']['type'][$i];
-			$file['tmp_name'] = $_FILES['fileImage']['tmp_name'][$i];
-			$file['error'] = $_FILES['fileImage']['error'][$i];
-			$file['size'] = $_FILES['fileImage']['size'][$i];
-			//function for upload the images
-			$mynewfile=move_uploaded_file($file['tmp_name'],"uploaded/".$file['name'])
-			
-			$i++;
-			
-			
-		}
+if (isset($_POST) && isset($_FILES['fileImage']['name'])) {
+    foreach ($_FILES['fileImage']['name'] as $i => $f) {
+        $file['name'] = $_FILES['fileImage']['name'][$i];
+        $file['type'] = $_FILES['fileImage']['type'][$i];
+        $file['tmp_name'] = $_FILES['fileImage']['tmp_name'][$i];
+        $file['error'] = $_FILES['fileImage']['error'][$i];
+        $file['size'] = $_FILES['fileImage']['size'][$i];
+        // function for upload the images
+        $mynewfile = move_uploaded_file($file['tmp_name'], 'uploaded/' . $file['name']);
+   }
 }
 ?>
 <form method="post" action="" enctype="multipart/form-data" id="frmImgUpload">
